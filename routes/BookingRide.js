157e009910dededb692eeb7a15a956f4cea3a49a
Mukela Katungu <65640620+Mukela12@users.noreply.github.com>
2024-05-27 10:35:07 +0800

    Update BookingRide.js

--- routes/BookingRide.js
+++ routes/BookingRide.js
@@ -7,18 +7,34 @@ import {
   startRide,
   endRide,
   searchDriversForBooking,
-  assignDriverToBooking
+  assignDriverToBooking,
+  getDriverBooking,
+  getDriverUser
 } from "../controllers/BookRideController.js";
+import { get } from "http";
 
 const router = express.Router();
 
+
+// user post routes
 router.post("/book-request", PassengerBookingRequest);
 
 router.post("/cancel-booking", cancelBooking);
 
 router.post("/search-driver",searchDriversForBooking);
 
-router.post("/select-driver", assignDriverToBooking)
+router.post("/select-driver", assignDriverToBooking);
+
+
+
+// Driver get routes
+router.get("/get-driver-booking", getDriverBooking);
+
+router.get("/get-user-driver", getDriverUser);
+
+
+
+// driver post routes
 
 router.post("/request-driver-cancellation", requestDriverCancellation);
 
